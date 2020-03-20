66997b40ad81af6e00c0105045fca5361e1bb1f1
Nikolay <mr.kolyamba99@yandex.ru>
2020-03-20 20:34:26 +0400

    добавил рандомный спаун и не до конца работающее движение оленей

--- WolvesAndDeer/WolvesAndDeer.cs
+++ WolvesAndDeer/WolvesAndDeer.cs
@@ -10,6 +10,12 @@ namespace WolvesAndDeer
     public class WolvesAndDeer : OSMLSModule
     {
         private Polygon wildLand;
+        private List<Deer> deers;
+        private List<Wolf> wolfs;
+        private Wolf wolf;
+        private Deer deer;
+        Random rnd = new Random();
+        
         protected override void Initialize()
         {
             //тут будут жить звери
@@ -23,28 +29,107 @@ namespace WolvesAndDeer
             };
 
             wildLand = new Polygon(new LinearRing(wildLandCoordinates));
-
             MapObjects.Add(wildLand);
-            GenerateAnimal(10);
-
+            
+            //Спаун оленей
+            deers = new List<Deer>();
+            foreach (var coordinate in GenerateCoordinatesAnimal(10))
+            {
+                var speedX = rnd.Next(-150, 150);
+                var speedY = rnd.Next(-150, 150); 
+                deer = new Deer(coordinate, speedX, speedY);
+                deers.Add(deer);
+                MapObjects.Add(deer);
+            }
+            
+            //Спаун волков
+            wolfs = new List<Wolf>();
+            foreach (var coordinate in GenerateCoordinatesAnimal(15))
+            {
+                wolf = new Wolf(coordinate);
+                wolfs.Add(wolf);
+                MapObjects.Add(wolf);
+            }
         }
 
         public override void Update(long elapsedMilliseconds)
         {
-            // throw new System.NotImplementedException();
+            var deers = MapObjects.GetAll<Deer>();
+            foreach (var deer in deers)
+            {
+                deer.MoveUpRight();   
+            }
         }
 
-        private void GenerateAnimal(int number)
+        private List<Coordinate> GenerateCoordinatesAnimal(int number)
         {
-            Random rnd = new Random(); //происходит троллинг из-за этой строки
-            //for (var i = 0; i < number; i++)
-            //{
-                //var coordinate = new Coordinate(rnd.Next(6625000, 7000000), rnd.Next(8710000, 9000000));
-                MapObjects.Add(new Point(6655000, 8910000));
-                //animals.Add(new Point(coordinate));
-           // }
+            var animalList = new List<Coordinate>();
+            for (var i = 0; i < number; i++)
+            {
+                var coordinate = new Coordinate(rnd.Next(6625000, 7000000), rnd.Next(8710000, 9000000));
+                animalList.Add(coordinate);
+            }
+            return animalList;
+        }
 
+        //Стиль для оленя
+        [CustomStyle(
+        @"new ol.style.Style({
+            image: new ol.style.Circle({
+                opacity: 1.0,
+                scale: 1.0,
+                radius: 3,
+                fill: new ol.style.Fill({
+                    color: 'rgba(0, 255, 0, 0.6)'
+                }),
+                stroke: new ol.style.Stroke({
+                    color: 'rgba(0, 0, 0, 0.4)',
+                    width: 1
+                }),
+            })
+        });
+        ")]
+        public class Deer : Point
+        {
+            public double _speedX { get; }
+            public double _speedY { get; }
 
+            public Deer (Coordinate coordinate, double speedX, double speedY) : base(coordinate)
+            {
+                _speedX = speedX;
+                _speedY = speedY;
+            }
+            
+            public void MoveUpRight()
+            {
+                X += _speedX;
+                Y += _speedY;
+            }
+        }
+
+        //Стиль для волка
+        [CustomStyle(
+            @"new ol.style.Style({
+            image: new ol.style.Circle({
+                opacity: 1.0,
+                scale: 1.0,
+                radius: 3,
+                fill: new ol.style.Fill({
+                    color: 'rgba(255, 0, 0, 0.6)'
+                }),
+                stroke: new ol.style.Stroke({
+                    color: 'rgba(0, 0, 0, 0.4)',
+                    width: 1
+                }),
+            })
+        });
+        ")]
+        public class Wolf : Point
+        {
+            public Wolf(Coordinate coordinate) : base(coordinate)
+            {
+                
+            }
         }
     }
 }
